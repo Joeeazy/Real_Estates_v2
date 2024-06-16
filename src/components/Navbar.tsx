b08aa5203f5c37627b3e48ee7c2038d9815ea1bf
Joeeazy <joeeazy34@gmail.com>
2024-06-16 14:09:35 +0300

    Mobile Navbar section complete

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,19 @@
+"use client";
 import { NAV_LINKS } from "@/constants/index";
 import React from "react";
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
 import Link from "../../node_modules/next/link";
 import { Button } from "./ui/button";
 
 export default function Navbar() {
+  const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
+
+  //function to toggle mobile navbar
+  const toggleNavbar = () => {
+    setMobileDrawerOpen(!mobileDrawerOpen);
+  };
+
   return (
     <nav className="fixed top-2 z-50 w-screen px-4">
       {/* navbar div  */}
@@ -42,7 +52,34 @@ export default function Navbar() {
           </Button>
           <Button className="bg-slate-800">Sign Up</Button>
         </div>
+        <div className="flex-col justify-end text-white md:flex lg:hidden">
+          <button onClick={toggleNavbar}>
+            {mobileDrawerOpen ? <X /> : <Menu />}
+          </button>
+        </div>
       </div>
+      {mobileDrawerOpen && (
+        <div className="rounded-md bg-black lg:hidden text-white">
+          <ul className="flex flex-col items-center">
+            {NAV_LINKS.map((item, index) => (
+              <li key={index} className="py-6 ">
+                <Link
+                  className="text-sm text-white hover:bg-neutral-500"
+                  href={item.url}
+                >
+                  {item.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <div className="flex items-center justify-center pb-8 text-white lg:hidden">
+            <Button variant="outline" className="mr-2 bg-black">
+              Sign In
+            </Button>
+            <Button className="bg-slate-800">Sign Up</Button>
+          </div>
+        </div>
+      )}
     </nav>
   );
 }
