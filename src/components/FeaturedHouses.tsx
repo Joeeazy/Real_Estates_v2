1a83a320ea397019fccfce29109897da91af258a
Joeeazy <joeeazy34@gmail.com>
2024-06-18 02:13:16 +0300

    Property section done

--- src/components/FeaturedHouses.tsx
+++ src/components/FeaturedHouses.tsx
@@ -1,19 +1,37 @@
 import { FEATURED_PROPERTIES } from "@/constants/index";
 import React from "react";
-
+import Image from "../../node_modules/next/image";
+import Link from "../../node_modules/next/link";
+import { Star } from "lucide-react";
 export default function FeaturedHouses() {
   return (
-    <div className="py-10 px-10">
-      <div className="grid md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4 space-x-10 items-center justify-between">
-        {FEATURED_PROPERTIES.map((item, index) => (
-          <div
-            key={index}
-            className="items-center px-20 justify-between grid grid-cols-4 md:grid-cols-2 sm:grid-cols-1 "
-          >
-            <div>{item.image}</div>
-            <div>
-              <h2>{item.name}</h2>
-            </div>
+    <div className="container mx-auto border-b py-6 pb-10">
+      <h2 className="text-center text-3xl tracking-tighter sm:text-4xl lg:text-5xl">
+        Featured Properties
+      </h2>
+      <div className="flex flex-wrap">
+        {FEATURED_PROPERTIES.map((property, index) => (
+          <div key={index} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+            <Link href="#" className="m-2 inline-block">
+              <div className="p-8">
+                <Image
+                  className="mb-8 rounded-md object-cover"
+                  src={property.image}
+                  width={300}
+                  height={300}
+                />
+                <div className="text-center">
+                  <h6 className="mb-5 mt-2 font-medium">{property.name}</h6>
+                  <span className="mb-5 mt-2">{property.price}</span>
+                  <div className="mt-2 flex items-center justify-center">
+                    <Star fill="gold" strokeWidth={0} className="mr-2" />
+                    <span className="text-sm">
+                      {property.rating} ({property.reviews} reviews)
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </Link>
           </div>
         ))}
       </div>
